f8ca868cda82bd0054d63716a6c9d7bbc75688f0
Megha Chhapre <95691127+MeghaChhapre@users.noreply.github.com>
2026-07-07 14:42:23 +0530

    d Banner

--- blocks/Banner/banner.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-const box = document.getElementById('bannerBox');
-const title = document.getElementById('title');
-
-btn.addEventListener('click', () => {
-    box.classList.add('active');
-    title.textContent = 'Success!';
-    btn.textContent = 'Done';
-});
